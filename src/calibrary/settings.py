e0d176547bdf505ed755ee4f237d978732be0554
Paolo <p.a.disanto@gmail.com>
2024-07-28 12:22:16 -0300

    cambios de Font propietaria, App funcionando previo a generacion y lectura de QR

--- src/calibrary/settings.py
+++ src/calibrary/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+#STATICFILES_DIRS = [BASE_DIR / "static"]
+#STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'core/static']
 #STATIC_ROOT = BASE_DIR / "staticfiles"
 
