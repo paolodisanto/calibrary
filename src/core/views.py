7c82410ca55ff49f3aa4d2d73dbab6ba1fd7c4e1
Paolo <p.a.disanto@gmail.com>
2024-08-24 18:12:32 -0300

    Corregido el manejo de erroes al escanear un QR inválido.

--- src/core/views.py
+++ src/core/views.py
@@ -1,30 +1,28 @@
-from django.http import HttpResponse
-from core.models import Instrument, SetUp
+from django.http import HttpResponseNotFound
+from core.models import Instrument
 from django.shortcuts import render, get_object_or_404
-
 
 def index(request):
     return render(request, "core/index.html")
 
 def get_instrument_context(tag_id):
     """
-    Funcion auxiliar para manejar la recuperacion de datos.
-    Me permite llamarla depues desde dos vistas (detail y intrumenet_detail)
+    Función auxiliar para manejar la recuperación de datos.
+    Me permite llamarla después desde dos vistas (detail e instrument_detail).
     """
-    instrument = get_object_or_404(Instrument, tag__id=tag_id)
+    try:
+        instrument = Instrument.objects.get(tag__id=tag_id)
+    except Instrument.DoesNotExist:
+        return None  # Indicar que no se encontró el instrumento
     
     checks = instrument.checks.all()
     contrasts = instrument.contrasts.all()
     setups = instrument.setups.all()
-    
-    
+
     latest_check = checks.order_by('-date').first()
     latest_contrast = contrasts.order_by('-date').first()
-    #latest_setup = setups.order_by('-date').first()
     latest_alarm_setup = setups.filter(alarm_set__isnull=False).order_by('-date').first()
     latest_trip_setup = setups.filter(trip_set__isnull=False).order_by('-date').first()
-    
-
 
     return {
         'instrument': instrument,
@@ -33,7 +31,6 @@ def get_instrument_context(tag_id):
         'setups': setups,
         'latest_check': latest_check,
         'latest_contrast': latest_contrast,
-        #'latest_setup': latest_setup,
         'latest_alarm': latest_alarm_setup,
         'latest_trip': latest_trip_setup,
     }
@@ -41,9 +38,16 @@ def get_instrument_context(tag_id):
 def search_item(request, qr_value=None):
     if not qr_value:
         qr_value = request.POST.get('qr_value')
+    
     context = get_instrument_context(qr_value)
+    if context is None:
+        return render(request, 'core/search_item.html', {'error_message': 'QR inválido. Por favor, intente nuevamente.'})
+    
     return render(request, 'core/detail.html', context)
 
 def instrument_detail(request, tag_id):
     context = get_instrument_context(tag_id)
+    if context is None:
+        return render(request, 'core/instrument_detail.html', {'error_message': 'Instrumento no encontrado. Por favor, intente nuevamente.'})
+    
     return render(request, 'core/instrument_detail.html', context)
